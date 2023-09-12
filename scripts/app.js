c1b2c18165232ccc90a03776df21b175cd033015
Aviral Verma <aviral.robin@gmail.com>
2023-09-12 09:04:28 +0530

    babel translation of template

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,10 @@
 console.log('App is running');
 
-var template = <h1>JSX is rendering</h1>;
+// var template = <h1 id="someID">JSX is rendering</h1>;
+var template = /*#__PURE__*/React.createElement("p", {
+  id: "someId"
+}, "JSX is rendering");
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
