5c1508f9548c476ee906c76e484c547dbc4ddfd3
Aviral Verma <aviral.robin@gmail.com>
2023-09-13 01:52:11 +0530

    created playeground

--- public/scripts/playground/play.js
+++ public/scripts/playground/play.js
@@ -16,20 +16,14 @@ function getSubtitle(subtitle) {
 var appTemplate = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
   id: "title"
 }, getTitle(app.title)), app.subtitle && getSubtitle(app.subtitle), /*#__PURE__*/React.createElement("p", null, app.options.length > 0 ? 'Here are the notes:' : 'No notes', " "), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, "Item 1"), /*#__PURE__*/React.createElement("li", null, "Item 2")));
-var user = {
-  name: 'Aviral Verma',
-  age: 26,
-  location: 'Mirzapur'
-};
-
-// JSX javascript XML
-var template = /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement("h1", {
-  id: "someID"
-}, "JSX is rendering, Change is now reflected"), /*#__PURE__*/React.createElement("h3", null, "Some info"), /*#__PURE__*/React.createElement("ol", null, /*#__PURE__*/React.createElement("li", null, user.name.toUpperCase()), user.age && user.age >= 18 && /*#__PURE__*/React.createElement("li", null, "age: ", user.age), /*#__PURE__*/React.createElement("li", null, "Software Developer"), /*#__PURE__*/React.createElement("li", null, "Location: ", user.location ? user.location : 'Banglore')));
+var count = 0;
+var template = /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", null, "Count: ", count, " "), /*#__PURE__*/React.createElement("button", {
+  id: "incrementor"
+}, "+1"));
 var appRoot = document.getElementById('app');
 
 // issue #3
 // import { createRoot } from 'react-dom/client';
 // const root = createRoot(appRoot); // createRoot(container!) if you use TypeScript
 
-ReactDOM.render(appTemplate, appRoot);
+ReactDOM.render(template, appRoot);
---
+++ src/playground/play.js
@@ -0,0 +1,44 @@
+console.log('App is running');
+
+var app = {
+    title: 'Notes App',
+    subtitle: 'Create short notes',
+    options: ['first', 'second'],
+}
+
+function getTitle(title) {
+    if(title) return title;
+    else return 'Notes';    
+}
+function getSubtitle(subtitle) {
+    if(subtitle) return <h3>{subtitle}</h3>;
+    else return undefined; // similar to not having an else statement    
+}
+
+var appTemplate = (
+    <>
+        <h1 id="title">{getTitle(app.title)}</h1>
+        {app.subtitle && getSubtitle(app.subtitle)}
+        <p>{app.options.length > 0 ? 'Here are the notes:': 'No notes'} </p>
+        <ol>
+            <li>Item 1</li>
+            <li>Item 2</li>
+        </ol>
+    </>
+);
+
+let count = 0;
+const template = (
+    <div>
+        <h2>Count: {count} </h2>
+        <button id="incrementor">+1</button>
+    </div>
+)
+
+var appRoot = document.getElementById('app');
+
+// issue #3
+// import { createRoot } from 'react-dom/client';
+// const root = createRoot(appRoot); // createRoot(container!) if you use TypeScript
+
+ReactDOM.render(template, appRoot);
