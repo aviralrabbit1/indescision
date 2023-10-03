f7afdee1a7de07c9a86dfd00422a8bc239119d06
Aviral Verma <aviral.robin@gmail.com>
2023-10-04 04:11:20 +0530

    Esc key and outside click exit Modal

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -7,6 +7,7 @@ const OptionModal = (props) => {
     isOpen={!!props.selectedOption}
     contentLabel='Selected Option'
     ariaHideApp={false}
+    onRequestClose={props.clearSelectedOption}
     >
         <h3>Selected Option: </h3>
         {props.selectedOption &&  <p> {props.selectedOption} </p> }
