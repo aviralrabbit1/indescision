3a4bb2a9fa8d36dded51f692a3252ce7449cef25
Aviral Verma <aviral.robin@gmail.com>
2023-10-04 04:21:58 +0530

    Withdraw Layour changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,15 @@ var _react = _interopRequireDefault(require("react"));
 var _reactDom = _interopRequireDefault(require("react-dom"));
 var _Indecision = _interopRequireDefault(require("./components/Indecision"));
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-var Layout = function Layout() {
-  return /*#__PURE__*/_react["default"].createElement(_react["default"].Fragment, null, /*#__PURE__*/_react["default"].createElement("h2", null, "Header"), /*#__PURE__*/_react["default"].createElement("hr", null), /*#__PURE__*/_react["default"].createElement(_Indecision["default"], null), /*#__PURE__*/_react["default"].createElement("hr", null), /*#__PURE__*/_react["default"].createElement("h2", null, "Footer"));
-};
-_reactDom["default"].render( /*#__PURE__*/_react["default"].createElement(Layout, null), document.getElementById('app'));
+// const Layout = () => {
+//     return (
+//         <>
+//             <h2>Header</h2>
+//             <hr/>
+//             <Indecision/>
+//             <hr/>
+//             <h2>Footer</h2>
+//         </>
+//     )
+// }
+_reactDom["default"].render( /*#__PURE__*/_react["default"].createElement(_Indecision["default"], null), document.getElementById('app'));
--- src/app.js
+++ src/app.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Indecision from './components/Indecision';
 
-const Layout = () => {
-    return (
-        <>
-            <h2>Header</h2>
-            <hr/>
-            <Indecision/>
-            <hr/>
-            <h2>Footer</h2>
-        </>
-    )
-}
+// const Layout = () => {
+//     return (
+//         <>
+//             <h2>Header</h2>
+//             <hr/>
+//             <Indecision/>
+//             <hr/>
+//             <h2>Footer</h2>
+//         </>
+//     )
+// }
 
-ReactDOM.render(<Layout/>, document.getElementById('app'));
+ReactDOM.render(<Indecision/>, document.getElementById('app'));
